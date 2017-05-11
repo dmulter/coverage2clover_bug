0b642f8ba660d99edc9426cc2c77656ca3c10d82
David Multer <dmulter@uaudio.com>
2017-05-11 08:22:28 -0700

    Initial commit

---
+++ showbug.sh
@@ -0,0 +1,3 @@
+:!/bin/bash
+py.test --cov one --cov two --cov-report xml tests/test.py
+coverage2clover -i coverage.xml
---
+++ tests/test.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_one():
+    assert True
+
+def test_two():
+    assert True
