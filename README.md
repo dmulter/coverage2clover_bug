1a68f17e0631be27d3232e13328596b755356dba
David Multer <dmulter@uaudio.com>
2017-05-12 08:27:51 -0700

    Add another showbug.sh test run to demonstrate a second component of the bug

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 
 Repo to demonstrate bug introduced in coverage 4.4. Just run showbug.sh.
 
+Add two runs of coverage2clover to demonstrate two aspects of the bug. One is due to the new 4.4
+feature that doesn't output an unambiguous filespec for two files (foo, bar) that another tool can
+use to open the file. The other shows a problem with two __init__ files in two modules that fails
+to output both.
+
 ## Issues
 
 - coverage2clover issue - https://github.com/tumb1er/coverage2clover/issues/10
--- showbug.sh
+++ showbug.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-py.test --cov one --cov two --cov-report xml tests/test.py
-coverage2clover -i coverage.xml
+
+py.test --cov one --cov two --cov-report xml:coverage1.xml tests/test.py
+py.test --cov one --cov two --cov-report xml:coverage2.xml --cov-config .coveragerc_noinitfiles tests/test.py
+
+echo "==== Running coverage2clover with two __init__.py files..."
+coverage2clover -i coverage1.xml
+
+echo "==== Running coverage2clover for foo.py and bar.py..."
+coverage2clover -i coverage2.xml
